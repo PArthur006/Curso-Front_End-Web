e6a3ed9720dba417d2f2570f19022f8a7bde8e90
P.Arthur <pedroarthurrodalm@gmail.com>
2024-07-24 17:52:03 -0300

    Códigos iniciais
    Sentenças de códigos e comentários.

---
+++ Seção 3-Inicio/comentarios.js
@@ -0,0 +1,12 @@
+//Console.log é usado para exibir algo na tela do computador!
+console.log("Passo 03");
+console.log("Passo 04")
+console.log("Passo 05") //MAIS UM EXEMPLO DE CONSOLE.LOG
+//console.log("Passo 02")
+
+/*
+* Esse é um
+* comentario
+* de múltiplas linhas
+*/
+console.log("Passo 02");
