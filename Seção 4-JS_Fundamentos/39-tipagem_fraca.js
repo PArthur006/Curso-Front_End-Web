170d6cfadc072198314114a10098370709d823a3
P.Arthur <pedroarthurrodalm@gmail.com>
2024-08-05 17:06:16 -0300

    Fundamentos do JavaScript - Passados

---
+++ Seção 4-JS_Fundamentos/39-tipagem_fraca.js
@@ -0,0 +1,13 @@
+let qualquer = 'Legal';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.3516;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+//Evitar nome genéricos e siglas
+/*
+let valor
+let numero
+*/
---
+++ Seção 4-JS_Fundamentos/41-Numeros_cuidados.js
@@ -0,0 +1,7 @@
+//ALGUNS CUIDADOS COM OPERAÇÕES
+
+console.log(7/0) //Retorna infinity
+console.log("10" / 2) //Realiza a divisão
+console.log("Show!" * 2) //NaN
+console.log(0.1 + 0.7) // Ele dá um número impreciso
+
