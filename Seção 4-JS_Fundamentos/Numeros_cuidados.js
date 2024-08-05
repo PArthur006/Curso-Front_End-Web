2456e4f25a93e2a00e421b9b9785c6fc7decac21
P.Arthur <pedroarthurrodalm@gmail.com>
2024-08-05 17:03:12 -0300

    Correção dos nomes - 2

--- Seção 4-JS_Fundamentos/Numeros_cuidados.js
+++
@@ -1,7 +0,0 @@
-//ALGUNS CUIDADOS COM OPERAÇÕES
-
-console.log(7/0) //Retorna infinity
-console.log("10" / 2) //Realiza a divisão
-console.log("Show!" * 2) //NaN
-console.log(0.1 + 0.7) // Ele dá um número impreciso
-
