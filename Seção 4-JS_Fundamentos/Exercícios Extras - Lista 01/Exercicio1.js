5a324bee29b31613bee45202602a1e2b86d4de73
P.Arthur <110242063+PArthur006@users.noreply.github.com>
2024-08-13 16:38:51 -0300

    Lista 01 de exercícios
    Lista de exercícios disponibilizada pelo professor;

---
+++ Seção 4-JS_Fundamentos/Exercícios Extras - Lista 01/Exercicio1.js
@@ -0,0 +1,10 @@
+/*  Exercício 1 
+    Crie uma função que retorna a string "Olá," concatenada com um argumento text (a ser passado para a função) e com ponto de exclamação "!" no final.
+*/
+
+function cumprimento(texto){
+    // return console.log(`Olá, ` + texto + `!`)
+    return console.log(`Olá, ${texto}!`)
+}
+
+cumprimento('Marcelo')
