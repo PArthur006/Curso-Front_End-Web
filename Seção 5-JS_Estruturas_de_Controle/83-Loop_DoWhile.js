f8ddebe319cf2e61e388854d7f08b84f0c93034c
P.Arthur <110242063+PArthur006@users.noreply.github.com>
2024-08-15 15:58:09 -0300

    Códigos dia 15/08 - Laços de repetição
    Laços: While, Do While, For, For In;
    Estrutura Switch;
    Uso de Break e continue.

---
+++ Seção 5-JS_Estruturas_de_Controle/83-Loop_DoWhile.js
@@ -0,0 +1,9 @@
+function getInteiroAleatorioEntre(min, max){
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+do{
+    var opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}`)
+}while (opcao != -1)
---
+++ Seção 5-JS_Estruturas_de_Controle/87-Break_Continue.js
@@ -0,0 +1,24 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (x in nums) {
+    if (x == 5){
+        break // Desvia o fluxo e interrompe todo o laço
+    }
+    console.log(`Índice ${x} = ${nums[x]}`)
+}
+console.log('')
+for (y in nums) {
+    if (y == 5){
+        continue // Desvia o fluxo e interrompe a parte atual do laço, pulando pro próximo
+    }
+    console.log(`Índice ${y} = ${nums[y]}`)
+}
+
+//  Cuidado com essa estrutura!
+externo: 
+for (a in nums){
+    for (b in nums){
+        if (a==2 && b == 3) break externo
+        console.log(`Par = ${a}, ${b}`)
+    }
+}
